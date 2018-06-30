c101d13e5a104b3731f111d48b8d51a42e7ca6e8
Cassidy <cassidy@Cassidys-iMac.local>
2018-06-30 19:51:45 -0400

    B1: resolved

---
+++ common.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // get current datetime
+    const date_output = document.getElementById("date_output");
+    let now = new Date();
+
+    date_output.innerHTML = now;
+});
+
+
+
+// select a historic date
+function makeDate() {
+}
