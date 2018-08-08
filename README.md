a940b7b2a26286b176f6d033d868b0e21ecfa6cb
Cassidy Brooke <cassidy@cassidybrooke.net>
2018-08-08 19:31:09 -0400

    rewriting

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # appfauna
 Data visualizations to understand fauna-vehicle collisions.
-
-![appfauna](https://github.com/cssidy/appfauna/blob/master/images/appfauna_web.jpg) 
-
-
-
--- assets/js/common.js
+++ assets/js/common.js
@@ -1,55 +1,52 @@
 
-
+// get geolocation, date and time from user's device
 
 document.addEventListener("DOMContentLoaded", function() {
+
     // get current datetime
     const date_output = document.getElementById("date_output");
     let now = new Date();
 
     date_output.innerHTML = now;
+
+
+    // geolocation
+    // Note: This example requires that you consent to location sharing when prompted by your browser.
+
+    function getGeo() {
+        const output = document.getElementById("geo_output");
+        if (!navigator.geolocation) {
+            output.innerHTML = "<p>Geolocation is not supported by your browser/device.</p>";
+            return;
+        }
+        function success(position) {
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+            output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+
+        }
+        function errorCallback(error) {
+            output.innerHTML = '<p>There was an error obtaining your position.</p>';
+            console.log('ERROR CODE: ' + error.code + ': ' + error.message);
+        }
+        output.innerHTML = "<p>Asking for permission...</p>";
+        navigator.geolocation.getCurrentPosition(success, errorCallback);
+    }
+    getGeo();
+
+
+
 });
 
-// select a historic date
-function makeDate() {
-}
 
 
 
 
 
-// GEOLOCATION
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser.
-
-function getGeo() {
-    const output = document.getElementById("geo_output");
-    if (!navigator.geolocation) {
-        output.innerHTML = "<p>Geolocation is not supported by your browser/device.</p>";
-        return;
-    }
-    function success(position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-    }
-    function errorCallback(error) {
-        output.innerHTML = '<p>There was an error obtaining your position.</p>';
-        console.log('ERROR CODE: ' + error.code + ': ' + error.message);
-    }
-    output.innerHTML = "<p>Asking for permission...</p>";
-    navigator.geolocation.getCurrentPosition(success, errorCallback);
-}
 
 
 
-/*
-function submitForm() {
-    let newData = new google.maps.LatLng(latitude, longitude);
-    heatmapData.push(newData);
-    console.log(heatmapData);
-}
-*/
+
 
 
 
